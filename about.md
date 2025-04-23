1a9222b14884208b5e61e2958e267c6f1577d6cd
user666 <user95401@gmail.com>
2025-04-23 20:48:54 +0400

    ya

--- about.md
+++ about.md
@@ -1,6 +1,8 @@
 # Playtime
 View your <cy>playtime</c> directly <cl>in-game</c>!
 
+Watch for <cl>open</c> button <cy>in stats</c> dropdown.
+
 This counts your <cl>in-game time</c> and <co>editor time</c>.
 
 Thank you to <cy>Zilko</c> for helping me make this <co>mod</c> :3
--- src/main.cpp
+++ src/main.cpp
@@ -3,27 +3,37 @@
 
 using namespace geode::prelude;
 
-#include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/StatsLayer.hpp>
 
-class $modify(MyMenuLayer, MenuLayer) {
-	bool init() {
-		if (!MenuLayer::init()) {
-			return false;
+class $modify(StatsLayerExt, StatsLayer) {
+	virtual void customSetup() {
+		StatsLayer::customSetup();
+
+		auto playtime = CCMenuItemExt::createSpriteExtraWithFrameName(
+			"GJ_timeIcon_001.png", 1.25f, [](CCMenuItem*) {
+				PlaytimeLayer::create()->show();
+			}
+		);
+		playtime->setID("playtime-button"_spr);
+
+		if (auto menu = this->m_buttonMenu) { //paranoic test
+			auto back = menu->getChildByType(0);
+			if (!back) return;
+			//add btn
+			menu->addChild(playtime);
+			//set layout if there is no one already
+			if (not menu->getLayout()) {
+				//fix point up to aaa... back btn?
+				menu->setPositionY(menu->getPositionY() + (back->getContentHeight() / 2));
+				menu->setAnchorPoint(CCPointMake(0.5f, 1.f));
+				menu->setLayout(
+					ColumnLayout::create()
+					->setAxisAlignment(AxisAlignment::End)
+					->setAxisReverse(true)
+				);
+			}
+			else menu->updateLayout();
+
 		}
-
-		auto myButton = CCMenuItemSpriteExtra::create(CircleButtonSprite::createWithSpriteFrameName("GJ_timeIcon_001.png", 0.85f, CircleBaseColor::Green, CircleBaseSize::MediumAlt), this, menu_selector(MyMenuLayer::onPlaytime));
-
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
-
-		myButton->setID("playtime-button"_spr);
-
-		menu->updateLayout();
-
-		return true;
-	}
-
-	void onPlaytime(CCObject*) {
-		PlaytimeLayer::create()->show();
 	}
 };
